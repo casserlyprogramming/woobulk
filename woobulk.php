5b37742e4b82cb85af27958a6f3aee150c40ccc2
Daniel Casserly <daniel@casserlyprogramming.com>
2017-08-17 13:27:45 +0100

    Added the main plugin file

---
+++ woobulk.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  Plugin Name: Woo Bulk Products
+  Plugin URI: https://github.com/casserlyprogramming/woobulk
+  Description: Bulk products for Woocommerce
+  Version: 1.0.0
+  Author: Daniel Casserly
+  Author URI: http://dandalfprogramming.blogspot.co.uk/
+ */
+
+
+if (!defined('ABSPATH'))
+    exit; // Exit 
+
+// Actions
+
+
+// Functions
+
+
